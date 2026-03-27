40f028de0ceb82b268be73387df6764a8ff30a3c
German <ramiromont2018@gmail.com>
2026-03-27 15:33:28 -0300

    🐛 Fix: Excluir ONNX runtime en Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+// Le decimos a Vercel que no toque los motores nativos de IA
+serverExternalPackages: ['onnxruntime-node', '@xenova/transformers'],
 };
 
 export default nextConfig;
